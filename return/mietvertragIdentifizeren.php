ec38a37b3eb49cd27e2a2ad25f3d7ab5574d5d8b
TimMiddeke <tim.middeke@studmail.w-hs.de>
2022-05-19 17:34:32 +0200

    Test Commit

--- return/mietvertragIdentifizeren.php
+++ return/mietvertragIdentifizeren.php
@@ -1,13 +1,19 @@
 <?php
-$con =   new MySQLi("localhost","root","","autovermietung");
+$con =   mysqli_connect("localhost","root","","autovermietung");
 if($con->connect_error){
     echo "Fehler bei  der Verbindung" . mysqli_connect_error();
     exit();
 }
-$erg = $con->query("SELECT * FROM mietvertraege")
+$sql = "SELECT * FROM mietvertraege";
+$db_erg = mysqli_query( $con, $sql );
+if ( ! $db_erg )
+{
+  die('Ungültige Abfrage: ' . mysqli_error());
+}
+
 echo "<center><table>"
 echo"<thead><tr><th>Mietvertragsnummer</th><th>Miete-Anfang</th> <th>Miete-Ende</th><th>Zahlart</th><th>Kosten</th><th>Ausgangszustand</th></tr></thead><tbody>";
-while($zeile = $erg->fetch_array())
+while ($zeile = mysqli_fetch_array( $db_erg, MYSQLI_ASSOC))
 {
     echo"<tr>
             <td>[$zeile['mietvertragID']</td>
@@ -25,4 +31,6 @@ while($zeile = $erg->fetch_array())
 echo"</tbody>
 </table>
 </center>";
+mysqli_free_result( $db_erg );
+mysqli_close($con);
 ?>
