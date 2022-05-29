7cd493fe1c1a35665815a3fd46937d8edeb7e45d
som-ould <56541608+som-ould@users.noreply.github.com>
2022-05-29 23:56:47 +0200

    new pro adress

--- functions/functions.php
+++ functions/functions.php
@@ -34,15 +34,15 @@ function send_mail($recipient,$subject, $message,$pathAttachment=null,$nameAttac
         $mail->isSMTP();
         $mail->Host='smtp.mail.yahoo.com';
         
-        $mail->Username='sihem.ouldmohand@yahoo.com';
-        $mail->Password='fytbevyafkbqzien';
+        $mail->Username='gamma_autovermietung@yahoo.com';
+        $mail->Password='njnzwgvpkcjmnsji';
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
         
         $mail->Port=587;
     
         $mail->SMTPAuth = true;
     
-        $mail->setFrom('sihem.ouldmohand@yahoo.com', 'Team Gamma');
+        $mail->setFrom('gamma_autovermietung@yahoo.com', 'Team Gamma');
     
         //recipient
         $mail->addAddress($recipient, '');
