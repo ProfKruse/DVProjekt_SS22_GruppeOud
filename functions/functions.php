732153b99c7e074212aa29be69848885db1dd610
TimMiddeke <tim.middeke@studmail.w-hs.de>
2022-05-31 10:01:36 +0200

    Ordnerstrukturanpassung

---
+++ functions/functions.php
@@ -0,0 +1,193 @@
+<?php
+    /* Klasse zur Behandlung von Ausnahmen und Fehlern */
+    require '../library/PHPMailer/src/Exception.php';
+    /* PHPMailer-Klasse */
+    require '../library/PHPMailer/src/PHPMailer.php';
+    /* SMTP-Klasse, die benötigt wird, um die Verbindung mit einem SMTP-Server herzustellen */
+    require '../library/PHPMailer/src/SMTP.php';
+
+    require '../library/pdfcreator/fpdf.php';
+    
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\SMTP;
+    use PHPMailer\PHPMailer\Exception;
+
+    function check_login($con){
+        if(isset($_SESSION['pseudo'])){      
+            $id = $_SESSION['pseudo'];
+            $query = "select * from kunden where pseudo = '$id' limit 1";
+    
+            $result = mysqli_query($con,$query);
+            if($result && mysqli_num_rows($result) > 0){
+                $user_data = mysqli_fetch_assoc($result);
+                return $user_data;
+            } 
+        }
+        header("Location: ../login/login.php");
+        die;
+    }
+    
+    
+    function send_mail($recipient,$subject, $message,$pathAttachment=null,$nameAttachment=null){
+        $mail=new PHPMailer(true);
+        try {
+            //settings
+    
+            $mail->isSMTP();
+            $mail->Host='smtp.mail.yahoo.com';
+            
+            $mail->Username='gamma_autovermietung@yahoo.com';
+            $mail->Password='njnzwgvpkcjmnsji';
+            $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+            
+            $mail->Port=587;
+        
+            $mail->SMTPAuth = true;
+        
+            $mail->setFrom('gamma_autovermietung@yahoo.com', 'Team Gamma');
+        
+            //recipient
+            $mail->addAddress($recipient, '');
+        
+            //content
+            $mail->isHTML(true); 
+            $mail->Subject = $subject;
+            $mail->Body= $message;
+            
+            if($pathAttachment != Null and $nameAttachment !=null){
+                $mail->addStringAttachment($pathAttachment, $nameAttachment);
+            }
+            
+            $mail->send();
+    
+    
+        } 
+        catch(Exception $e) {
+            echo 'Email wurde nicht gesendet';
+            echo 'Mailer Error: '.$mail->ErrorInfo;
+        }
+    }
+
+    function mietvertragsAnzeige($mietvertragsdaten,$con){
+        #session_start(); 
+        if (isset($mietvertragsdaten['mietvertragid'])) { 
+            $_SESSION['mietvertragid'] = $mietvertragsdaten["mietvertragid"]; 
+            $statement = "SELECT * FROM mietvertraege WHERE mietvertragID =" . $_SESSION['mietvertragid']; 
+            $db_erg = mysqli_query( $con, $statement ); 
+            if (!$db_erg ) 
+            { 
+                die('Fehler in der SQL Anfrage'); 
+            } 
+            $count =  0;                 
+            while ($zeile = mysqli_fetch_array( $db_erg, MYSQLI_ASSOC)) 
+            { 
+                $count = $count+1; 
+                echo"<center> 
+                <table class='mietdaten'> 
+                    <thead> 
+                        <tr> 
+                            <th>Mietvertragsnummer</th> 
+                            <th>Status</th> 
+                            <th>Mietdauer in Tagen</th> 
+                            <th>Mietgebuehr</th> 
+                            <th>Zahlart</th> 
+                            <th>Abholstation</th> 
+                            <th>Rueckgabestation</th> 
+                            <th>Vertragsnummer</th> 
+                            <th>Kundennummer</th>
+                        </tr> 
+                    </thead> 
+                    <tbody> 
+                        <tr> 
+                            <td>{$zeile['mietvertragID']}</td> 
+                            <td>{$zeile['status']}</td> 
+                            <td>{$zeile['mietdauerTage']}</td> 
+                            <td>{$zeile['mietgebuehr']}</td>     
+                            <td>{$zeile['zahlart']}</td> 
+                            <td>{$zeile['abholstation']}</td> 
+                            <td>{$zeile['rueckgabestation']}</td> 
+                            <td>{$zeile['vertragID']}</td> 
+                            <td>{$zeile['kundeID']}</td> 
+                        </tr>  
+                    </tbody> 
+                </table> 
+                </center>"; 
+                } 
+                if($count ==  0) 
+                { 
+                    echo "<p>Die eingegebene ID existiert nicht in der DB</p>";
+                    $_SESSION['mietvertragID'] = null;
+                }
+                mysqli_free_result( $db_erg ); 
+                mysqli_close($con); 
+            } 
+        }
+
+    function sendeRuecknahmeprotokoll($nutzungsdaten,$con,$mietvertragsid=null)
+        {  
+            $email = "";
+            if (isset($nutzungsdaten['tank'])) 
+            {
+                $tank = trim($nutzungsdaten['tank']);
+                if (isset($nutzungsdaten['kilometerstand'])) 
+            {
+                $kilometerstand = trim($nutzungsdaten['kilometerstand']);
+                if (isset($nutzungsdaten['sauberkeit'])) 
+                {
+                    $sauberkeit = trim($nutzungsdaten['sauberkeit']);
+                    if (isset($nutzungsdaten['mechanik'])) 
+                    {
+                        $mechanik = trim($nutzungsdaten['mechanik']);
+                        if (isset($mietvertragsid)) 
+                        {
+                            try {
+                                $statement = "INSERT INTO ruecknahmeprotokolle (ersteller,tank,sauberkeit, mechanik, kilometerstand, mietvertragID) VALUES (1,'$tank','$sauberkeit','$mechanik','$kilometerstand','$mietvertragsid')"; 
+                                $ergebnis = $con->query($statement);
+                            } catch (mysqli_sql_exception $e) {
+                                if ($e->getCode() == 1062) {
+                                    echo "Dopplungsfehler";
+                                    // Duplicate user
+                                } else {
+                                    throw $e;// in case it's any other error
+                                }
+                            
+                            } 
+                            //$statement = "UPDATE kfzs SET kilometerStand = " . $kilometerstand "WHERE ;"; 
+                            //$ergebnis = $con->query($statement);
+                            $email = "tm.middeke@gmx.de";#$con->query("SELECT emailAdresse FROM kunden WHERE kundeID=" . $_SESSION['mietvertragID']);                          
+                            mysqli_close($con);
+                            $subject = 'Ruecknahmeprotokoll';
+                            $message = '<!DOCTYPE html>
+                            <html>
+                            <body>
+                            <p>Sehr geehrter Kunde,</p>
+                            <p>anbei erhaelst du dein Ruecknahmeprotokoll. ;)</p>
+                            <br>
+                            <p>Mit lieben Gruessen</p>
+                            <p>Dein Pascal</p>
+                            </body>
+                            </html>';
+                            $pdf = new FPDF();
+                            $pdf->AddPage();
+                            $pdf->SetFont('Arial','B',16);
+                            $pdf->Cell(0,10,'Ruecknahmeprotokoll:',0,1);
+                            $pdf->SetFont('Arial','B',12);
+                            $pdf->Cell(0,10,'Der Tank war noch zu: ' . $tank . '% gefuellt.',1,1);
+                            $pdf->Cell(0,10,'Das Auto war ' . $sauberkeit . '.',1,1);
+                            $pdf->Cell(0,10,'Die Mechanik war ' . $mechanik . '.',1,1);
+                            $pdf->Cell(0,10,'Du bist ' . $kilometerstand . 'km gefahren.',1,1);
+                            $pdf->Cell(0,10,'Deine Mietvertragsnummer ist: ' . $_SESSION['mietvertragID'] . '.',1,1);
+                            $doc = $pdf->Output('S');
+                            send_mail($email, $subject, $message,$doc,"Ruecknahmeprotokoll.pdf");
+                            die;                             
+                        }
+                        else
+                        {
+                            echo "<p>Die eingegebene ID existiert nicht in der DB oder es wurde bereits ein Ruecknahmeprotokoll erzeugt, bitte geben Sie eine korrekte ein und geben Sie dann erneut die nutzungsrelevanten Daten ein.</p>";
+                        }
+                    }
+                }                       
+            }              
+        }
+    }
+?>
--- return/return_dialog.php
+++ return/return_dialog.php
@@ -1,134 +1,8 @@
 <!DOCTYPE html> 
     <?php
-        session_start();
-        function mietvertragsAnzeige(){ 
-            include("Database/db_inc.php"); 
-            if (isset($_POST['mietvertagid'])) { 
-                $_SESSION['mietvertragID'] = $_POST["mietvertagid"]; 
-                $statement = "SELECT * FROM mietvertraege WHERE mietvertragID =" . $_SESSION['mietvertragID']; 
-                $db_erg = mysqli_query( $con, $statement ); 
-                if (!$db_erg ) 
-                { 
-                    die('Fehler in der SQL Anfrage'); 
-                } 
-                $count =  0;                 
-                while ($zeile = mysqli_fetch_array( $db_erg, MYSQLI_ASSOC)) 
-                { 
-                    $count = $count+1; 
-                    echo"<center> 
-                    <table class='mietdaten'> 
-                        <thead> 
-                            <tr> 
-                                <th>Mietvertragsnummer</th> 
-                                <th>Status</th> 
-                                <th>Mietdauer in Tagen</th> 
-                                <th>Mietgebuehr</th> 
-                                <th>Zahlart</th> 
-                                <th>Abholstation</th> 
-                                <th>Rueckgabestation</th> 
-                                <th>Vertragsnummer</th> 
-                                <th>Kundennummer</th>
-                            </tr> 
-                        </thead> 
-                        <tbody> 
-                            <tr> 
-                                <td>{$zeile['mietvertragID']}</td> 
-                                <td>{$zeile['status']}</td> 
-                                <td>{$zeile['mietdauerTage']}</td> 
-                                <td>{$zeile['mietgebuehr']}</td>     
-                                <td>{$zeile['zahlart']}</td> 
-                                <td>{$zeile['abholstation']}</td> 
-                                <td>{$zeile['rueckgabestation']}</td> 
-                                <td>{$zeile['vertragID']}</td> 
-                                <td>{$zeile['kundeID']}</td> 
-                            </tr>  
-                        </tbody> 
-                    </table> 
-                    </center>"; 
-                    } 
-                    if($count ==  0) 
-                    { 
-                        echo "<p>Die eingegebene ID existiert nicht in der DB</p>";
-                        $_SESSION['mietvertragID'] = null;
-                    }
-                    
- 
-                    mysqli_free_result( $db_erg ); 
-                    mysqli_close($con); 
-                } 
-            }
-
-        function sendeRuecknahmeprotokoll()
-        {  
-            include("Database/db_inc.php");
-            include("functions/function.php");
-            require('pdfcreator/fpdf.php');
-            $email = "";
-            if (isset($_POST['tank'])) 
-            {
-                $tank = trim($_POST['tank']);
-                if (isset($_POST['kilometerstand'])) 
-            {
-                $kilometerstand = trim($_POST['kilometerstand']);
-                if (isset($_POST['sauberkeit'])) 
-                {
-                    $sauberkeit = trim($_POST['sauberkeit']);
-                    if (isset($_POST['mechanik'])) 
-                    {
-                        $mechanik = trim($_POST['mechanik']);
-                        if (isset($_SESSION['mietvertragID'])) 
-                        {
-                            try {
-                                $statement = "INSERT INTO ruecknahmeprotokolle (ersteller,tank,sauberkeit, mechanik, kilometerstand, mietvertragID) VALUES (1,'$tank','$sauberkeit','$mechanik','$kilometerstand',". $_SESSION['mietvertragID'].")"; 
-                                $ergebnis = $con->query($statement);
-                            } catch (mysqli_sql_exception $e) {
-                                if ($e->getCode() == 1062) {
-                                    // Duplicate user
-                                } else {
-                                    throw $e;// in case it's any other error
-                                }
-                            
-                            } 
-                            //$statement = "UPDATE kfzs SET kilometerStand = " . $kilometerstand "WHERE ;"; 
-                            //$ergebnis = $con->query($statement);
-                            $email = "tm.middeke@gmx.de";#$con->query("SELECT emailAdresse FROM kunden WHERE kundeID=" . $_SESSION['mietvertragID']);                          
-                            mysqli_close($con);
-                            $subject = 'Ruecknahmeprotokoll';
-                            $message = '<!DOCTYPE html>
-                            <html>
-                            <body>
-                            <p>Sehr geehrter Kunde,</p>
-                            <p>anbei erhaelst du dein Ruecknahmeprotokoll. ;)</p>
-                            <br>
-                            <p>Mit lieben Gruessen</p>
-                            <p>Dein Pascal</p>
-                            </body>
-                            </html>';
-                            $pdf = new FPDF();
-                            $pdf->AddPage();
-                            $pdf->SetFont('Arial','B',16);
-                            $pdf->Cell(0,10,'Ruecknahmeprotokoll:',0,1);
-                            $pdf->SetFont('Arial','B',12);
-                            $pdf->Cell(0,10,'Der Tank war noch zu: ' . $tank . '% gefuellt.',1,1);
-                            $pdf->Cell(0,10,'Das Auto war ' . $sauberkeit . '.',1,1);
-                            $pdf->Cell(0,10,'Die Mechanik war ' . $mechanik . '.',1,1);
-                            $pdf->Cell(0,10,'Du bist ' . $kilometerstand . 'km gefahren.',1,1);
-                            $pdf->Cell(0,10,'Deine Mietvertragsnummer ist: ' . $_SESSION['mietvertragID'] . '.',1,1);
-                            $doc = $pdf->Output('S');
-                            send_mail($email, $subject, $message,$doc,"Ruecknahmeprotokoll.pdf");
-                            die;                             
-                        }
-                        else
-                        {
-                            echo "<p>Die eingegebene ID existiert nicht in der DB oder es wurde bereits ein Ruecknahmeprotokoll erzeugt, bitte geben Sie eine korrekte ein und geben Sie dann erneut die nutzungsrelevanten Daten ein.</p>";
-                        }
-                    }
-                }                       
-            }
-            session_destroy();                
-        }
-
-    }
+        require("../Database/db_inc.php");
+        require("../functions/functions.php");
+        session_start();        
     ?> 
 <html> 
     <head> 
@@ -159,7 +33,7 @@
                 <!--------------------------------------------------------------> 
                 <div class="group"> 
                     <label for="mietvertragsnummer"><b>*Mietvertragsnummer</b></label> 
-                    <input type="number" name="mietvertagid" id="mietvertagid" max="99999999999" min="1"  required > 
+                    <input type="number" name="mietvertragid" id="mietvertragid" max="99999999999" min="1"  required > 
                 </div> 
                  
                 <div class="group"> 
@@ -168,7 +42,9 @@
                 </div> 
             </form> 
             <?php 
-                mietvertragsAnzeige(); 
+                mietvertragsAnzeige($_POST,$con);
+                if(isset($_POST['mietvertragid']))
+                    $_SESSION['mietvertragid'] = $_POST['mietvertragid']; 
             ?> 
             <form action="return_dialog.php" method="POST"> 
                 <div class="group">                   
@@ -192,8 +68,9 @@
                 </div> 
                 <br>
                 <button type="submit">Protokoll erzeugen</button></div>
-                <?php 
-                    sendeRuecknahmeprotokoll(); 
+                <?php
+                    sendeRuecknahmeprotokoll($_POST,$con,$_SESSION['mietvertragid']);
+                    #session_destroy(); 
                 ?>  
             </form> 
             </div> 
