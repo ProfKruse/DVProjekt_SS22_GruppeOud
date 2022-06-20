c2a03ac2e771e7021a9066578ecc12662de4e980
som-ould <56541608+som-ould@users.noreply.github.com>
2022-06-20 14:20:43 +0200

    password generieren

---
+++ Database/password_generieren.php
@@ -0,0 +1,19 @@
+<?php
+
+    include("../database/db_inc.php");
+    include("../functions/functions.php");
+
+    $result = $con -> query("SELECT kundeid FROM Kunden");
+    echo "Returned rows are: " . $result -> num_rows;
+    $new_password = "password";
+    while($kunde = $result->fetch_array(MYSQLI_ASSOC)):
+        foreach($kunde as $value) {
+           echo $value . '<br>';
+           $hash = password_hash($new_password, PASSWORD_DEFAULT);
+           $stmt = "update kunden set password = '$hash' where kundeid =".$value;
+           $jiji = mysqli_query($con, $stmt);
+           echo "done";
+        }
+    endwhile;
+    $con->close();
+?>
